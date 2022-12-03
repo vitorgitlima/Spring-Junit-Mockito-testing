9f6ab5758ccf1f6dc2276bf9e0d6bdeb7bc99695
Vitor Lima <CIANDT\vitorgl@lnb029367bhz.ciandt.global>
2022-12-03 18:41:21 -0300

    Setting up Business Service to call a Data Service

---
+++ src/main/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessImpl.java
@@ -0,0 +1,11 @@
+package com.in28minutes.unittesting.unittesting.business;
+
+public class SomeBusinessImpl {
+    public int calculateSum(int[] data){
+        int sum = 0;
+        for(int value:data){
+            sum += value;
+        }
+        return sum;
+    }
+}
